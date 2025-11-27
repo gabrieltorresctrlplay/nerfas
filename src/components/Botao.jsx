64daaf13d203f5619b1bd656516cf954a6f26cbc
Gabriel Rodrigues Torres <gabriel.torres.ctrlplay@gmail.com>
2025-11-27 15:15:19 -0300

    cleanup

--- src/components/Botao.jsx
+++
@@ -1,7 +0,0 @@
-export const Botao = props => {
-  return (
-    <button className='cursor-pointer rounded-lg border-b-4 border-blue-800 bg-blue-600 px-6 py-2 font-bold text-white shadow-lg transition-all hover:border-blue-900 hover:bg-blue-700 active:scale-95'>
-      {props.texto}
-    </button>
-  )
-}
